0599f8b17d3e9a7624dabf0a6de046607fc8766d
66OKAY <okiramadhan120@gmail.com>
2026-09-13 15:14:41 +0700

    update nambah poto

--- components/RomanticLanding.tsx
+++ components/RomanticLanding.tsx
@@ -223,7 +223,7 @@ const galleryImages = [
   },
   {
     src: "/images/43(1).jpg",
-    alt: "Jalan2 malem berkedok nolongin ejang",
+    alt: "Jalan2 malem berkedok jemput ejang",
     message: "Awalnya kita gabut Malem takbiran lebaran Haji ga ngapa2in tadinya mau nnton film lewat zoom tapi zoom nya gabisa nampilin gambar, nah selang beberapa menit lu ngabarin kalo ejang motornya mogok gapake lama kita meluncur deh jemput ejang sambil jalan2 malem heheheh.",
   },
 ];
